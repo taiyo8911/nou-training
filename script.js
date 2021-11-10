d205568520997d59e2aae6b038465152881b18bd
taiyo8911 <68884687+taiyo8911@users.noreply.github.com>
2021-11-10 13:02:07 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const text = [
     "緑",
     "青",
     "白",
+    "黄",
 ]
 
-const color = [
+const text_color = [
     "red",
     "green",
     "blue",
     "white",
+    "yellow",
 ]
 
 const answer_color = {
@@ -17,25 +19,33 @@ const answer_color = {
     "green": "緑",
     "blue": "青",
     "white": "白",
+    "yellow": "黄",
+
 }
 
-// 乱数を作る
-function getRandom() {
-    let random = Math.floor(Math.random() * text.length);
+
+
+// 乱数を作る関数
+function getRandom(min, max) {
+    let random = Math.floor(Math.random() * max + min);
     return random;
 }
 
+
 // 画面にランダムな文字を表示する
-let random_text = getRandom();
-document.getElementById("text").innerText = text[random_text];
+let random_text = text[getRandom(0, text.length)];
+document.getElementById("text").innerText = random_text;
 
 
-// 文字色をランダムに変える
-let random_color = getRandom();
-document.getElementById("text").style.color = color[random_color];
+// 文字の色をランダムな色に変える
+let random_color = text_color[getRandom(0, text_color.length)];
+document.getElementById("text").style.color = random_color;
 
 
 // 文字の「色の名前」をランダムな場所に表示する
-let ans_display_number = getRandom() + 1;
-let num = random_color;
-document.getElementById(`select${ans_display_number}`).innerText = answer_color[color[num]];
+
+// 1-4のランダムな数値を取得する
+let random_num = getRandom(1, 4);
+
+// 正解の色名をランダムな位置に表示する。
+document.getElementById(`select${random_num}`).innerText = answer_color[random_color];
